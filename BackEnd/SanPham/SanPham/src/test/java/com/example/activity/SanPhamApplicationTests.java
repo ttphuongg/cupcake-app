9cf6f4a28e07c6a3a5551bb26a7a347d879856d3
Nguyen Hung Long <129longnguyen@gmail.com>
2026-04-20 13:48:43 +0700

    add backend SanPham

---
+++ BackEnd/SanPham/SanPham/src/test/java/com/example/activity/SanPhamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanPhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
