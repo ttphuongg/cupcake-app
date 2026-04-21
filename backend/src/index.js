a51ec7731ce87107e1bca11bf06eef27229201c7
phuong <trannthuphuong@gmail.com>
2026-04-21 23:54:55 +0700

    setup: frontend + backend running

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
