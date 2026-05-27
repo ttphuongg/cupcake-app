7e2fa335900cb7d4b088e85e214b87d38c42fd1b
chutranduc205 <chuduc908@gmail.com>
2026-05-28 01:26:56 +0700

    Đức done

--- frontend/constants/config.ts
+++ frontend/constants/config.ts
@@ -2,5 +2,5 @@
 
 export const API_URL = 'http://192.168.0.107'; 
 // export const API_URL = 'http://172.20.10.5';
-export const BASE_URL = `${API_URL}:3000/api/v1`;
+export const BASE_URL = `${API_URL}:5000/api/v1`;
 export const TIMEOUT = 10000; // 10 giây
